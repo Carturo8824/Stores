3fa9452120d3c645ff124304a657332f0ce98d9c
Cristy Arturo <cristy8824@gmail.com>
2024-07-03 14:44:23 -0600

    feat(Order): Agregando ValueObject OrderDetail

---
+++ Stores.Sales.Backend.BusinessObjects/ValueObjects/OrderDetail.cs
@@ -0,0 +1,8 @@
+﻿}namespace Stores.Sales.Backend.BusinessObjects.ValueObjects;
+
+public class OrderDetail(int productId, decimal unitPrice, short quantity)
+{
+    public int ProductId => productId;
+    public decimal UnitPrice => unitPrice;
+    public short Quantity => quantity;
+}
