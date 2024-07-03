15ba945b32314dcf71b50124448fee8a93ef3647
Cristy Arturo <cristy8824@gmail.com>
2024-07-03 15:28:58 -0600

    feat(CreateOrder): Agregago Interfaces para InputPort y OutPort

--- Stores.Sales.Backend.BusinessObjects/GlobalUsings.cs
+++ Stores.Sales.Backend.BusinessObjects/GlobalUsings.cs
@@ -2,3 +2,4 @@ global using Stores.Sales.Backend.BusinessObjects.Enums;
 global using Stores.Sales.Backend.BusinessObjects.POCOEntities;
 global using Stores.Sales.Backend.BusinessObjects.ValueObjects;
 global using Stores.Sales.Entities.DTOs.CreateOrder;
+global using Stores.Sales.Backend.BusinessObjects.Aggregates;
