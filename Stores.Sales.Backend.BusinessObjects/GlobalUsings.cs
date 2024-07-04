74e2ee004a45c9e5b8020f12e7b620eebed6aa56
Cristy Arturo <cristy8824@gmail.com>
2024-07-04 09:05:08 -0600

    feat(CreateOrder): Agregago Interfaces para manejar el repositorio de Crear la orden

--- Stores.Sales.Backend.BusinessObjects/GlobalUsings.cs
+++ Stores.Sales.Backend.BusinessObjects/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using Stores.Sales.Backend.BusinessObjects.POCOEntities;
 global using Stores.Sales.Backend.BusinessObjects.ValueObjects;
 global using Stores.Sales.Entities.DTOs.CreateOrder;
 global using Stores.Sales.Backend.BusinessObjects.Aggregates;
+global using Stores.Sales.Backend.BusinessObjects.Interfaces.Common;
