6a614f4e270e9932256e80591d7e35305c079262
Cristy Arturo <cristy8824@gmail.com>
2024-07-05 10:19:10 -0600

    feat(repositories): Agrego el contexto de datos  Changes to be committed: 	new file:   Stores.Sales.Backend.EFCore/DataContexts/StoresContext.cs 	new file:   Stores.Sales.Backend.EFCore/GlobalUsings.cs

---
+++ Stores.Sales.Backend.EFCore/DataContexts/StoresContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+using Stores.Sales.Backend.EFCore.Options;
+
+namespace Stores.Sales.Backend.EFCore.DataContexts;
+
+internal class StoresContext : DbContext
+{
+    readonly IOptions<DBOptions> DBOptions;
+    public StoresContext(IOptions<DBOptions> dbOptions) =>
+        DBOptions = dbOptions;
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    protected override void OnConfiguring(
+DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            DBOptions.Value.ConnectionString);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            Assembly.GetExecutingAssembly());
+    }
+}
