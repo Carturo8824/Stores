f05e6b5e0d977d5f7c9bca4a49b5cb6c0138bece
Cristy Arturo <cristy8824@gmail.com>
2024-07-05 12:53:53 -0600

    feat(repositories): Agrego la implementacion del ICommandsRepository  Changes to be committed: 	modified:   Stores.Sales.Backend.EFCore/GlobalUsings.cs 	new file:   Stores.Sales.Backend.EFCore/Repositories/CommandsRepository.cs

--- Stores.Sales.Backend.EFCore/GlobalUsings.cs
+++ Stores.Sales.Backend.EFCore/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Stores.Sales.Backend.EFCore.Entities;
 global using System.Reflection;
+global using Stores.Sales.Backend.BusinessObjects.Aggregates;
+global using Stores.Sales.Backend.BusinessObjects.Interfaces.Repositories;
+global using Stores.Sales.Backend.EFCore.DataContexts;
---
+++ Stores.Sales.Backend.EFCore/Repositories/CommandsRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Stores.Sales.Backend.EFCore.Repositories;
+
+internal class CommandsRepository(StoresContext context) :
+    ICommandsRepository
+{
+    public async Task CreateOrder(OrderAggregate order)
+    {
+        await context.AddAsync(order);
+        await context.AddRangeAsync(order.OrderDetails
+            .Select(d => new OrderDetail
+            {
+                Order = order,
+                ProductId = d.ProductId,
+                Quantity = d.Quantity,
+                UnitPrice = d.UnitPrice
+            }).ToArray());
+    }
+    public async Task SaveChanges() =>
+        await context.SaveChangesAsync();
+}
